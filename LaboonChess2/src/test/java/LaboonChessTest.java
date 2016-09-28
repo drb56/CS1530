0658d1735942a59f8e87072849f3a71cd060403b
luckyleap <david.r.tsui@gmail.com>
2016-09-28 14:58:05 -0400

    Added gradle details

---
+++ LaboonChess2/src/test/java/LaboonChessTest.java
@@ -0,0 +1,14 @@
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by david on 9/28/16.
+ */
+public class LaboonChessTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+}
