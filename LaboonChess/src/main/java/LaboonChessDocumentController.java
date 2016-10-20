f8c4d09a9dd1996adf5c098c95f3cadf772a2278
Joe Meszar <github@outk.st>
2016-10-20 00:48:34 -0400

    Added methods for updating the 2D chessboard array
    Not entirely sure if we are going to be using this 2D array yet,
    but it was something that was talked about so I wanted to implement
    it before further branching occurs.
    
    - update2DArrayChessboard() method takes a SAN notation of the starting
    	chess square and the ending chess square (first click -> second click)
    	and updates the 2D array accordingly. Uses helper methods named
    	sanTo2DRow() and sanTo2DCol().
    
    - sanTo2DRow() method takes the SAN notation of a single chess square
    	and manipulates its ROW designation (1-8) from USCF standards to
    	zero-based array indexing standards.
    
    - sanTo2DCol() method takes the SAN notation of a single chess square
    	and manipulates its COLUMN designation (a-h) from USCF standards to
    	zero-based array indexing standards.

--- LaboonChess/src/main/java/LaboonChessDocumentController.java
+++ LaboonChess/src/main/java/LaboonChessDocumentController.java
@@ -43,12 +43,25 @@ public class LaboonChessDocumentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        /* start the Stockfish binary */
         stockfish = new Stockfish();
         if (stockfish.startEngine()) {
             System.out.println("Engine has started..");
         } else {
             System.out.println("Oops! I did it again..");
         }
+
+        /* build the chessboard */
+        chessboard = new char[][]{
+                { 'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r' },
+                { 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p' },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0, 0, 0, 0 },
+                { 'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P' },
+                { 'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R' }
+        };
     }
 
     @FXML
@@ -120,8 +133,27 @@ public class LaboonChessDocumentController implements Initializable {
         lblStatus.setText("Undo Move menu item clicked");
     }
 
+    /**
+     *  Handles the logic for when a user "plays" on the GUI chessboard. All "plays" are done by
+     *      using mouseclick events. Each chessboard square (Pane) on the GUI is tied to this
+     *      method to use for its mouseclick event.
+     *
+     *      Whenever a mouseclick occurs, there will be two distinct types:
+     *          1. "First Click"
+     *              - See if a chess piece exists on this square.
+     *              - If no piece, then do nothing. Wait for "First Click" again.
+     *              - If a piece exists, store it and wait for "Second Click".
+     *
+     *          2. "Second Click"
+     *              - Determine if this second click location is a VALID chess move.
+     *                  - Based off the "First Click" and what type of chess piece.
+     *              - If valid, move the chess piece from the "First Click".
+     *                  location to the "Second Click" location on the chess board.
+     *
+     * @param event The mouseclick event. Holds the GUI object that was clicked.
+     */
     @FXML void handleChessboardClickAction(MouseEvent event) {
-        Pane curSquare = (Pane) event.getSource();                          // get the source chess square that was clicked
+        Pane curSquare = (Pane) event.getSource();                              // get the source chess square that was clicked
 
         /*
             If this is the "first click", then we need to get
@@ -133,14 +165,14 @@ public class LaboonChessDocumentController implements Initializable {
         if (isFirstClick) {
             if (curSquare.getChildren().isEmpty()) {
                 /* DO NOTHING: no chess piece here */
-                isFirstClick = true;                                           // reset
+                isFirstClick = true;                                            // reset
 
             } else {
                 /* FIRST-CLICK: set up for the second click */
-                guiChessSquare = curSquare;                                    // hold reference to this square
-                guiChessPiece = (ImageView) curSquare.getChildren().get(0);    // hold reference to this piece
-                guiChessPiece.setOpacity(.6);                                  // set opacity to make it look "selected"
-                isFirstClick = false;                                          // now wait for second-click
+                guiChessSquare = curSquare;                                     // hold reference to this square
+                guiChessPiece = (ImageView) curSquare.getChildren().get(0);     // hold reference to this piece
+                guiChessPiece.setOpacity(.6);                                   // set opacity to make it look "selected"
+                isFirstClick = false;                                           // now wait for second-click
             }
         } else {
             /* SECOND-CLICK */
@@ -150,40 +182,105 @@ public class LaboonChessDocumentController implements Initializable {
             if (curSquare.getChildren().isEmpty()) {
                 /* EMPTY SQUARE */
 
-                curSquare.getChildren().add(0, guiChessPiece);         // place the chess piece here
-                san = guiChessSquare.getId() + curSquare.getId();      // get the move in terms of SAN (e.g. e3d6)
+                curSquare.getChildren().add(0, guiChessPiece);          // place the chess piece here
+                san = guiChessSquare.getId() + curSquare.getId();       // get the move in terms of SAN (e.g. e3d6)
 
-            } else if (curSquare.getChildren().get(0).getId().matches("[a-z]") != guiChessPiece.getId().matches("[a-z]")) {
+                update2DArrayChessboard(guiChessSquare.getId(), curSquare.getId());     // keep 2D chessboard array updated
+
+            } else if (curSquare.getChildren().get(0).getId().matches("[a-z]")
+                    != guiChessPiece.getId().matches("[a-z]")) {
+
                 /* OPPONENT PIECE EXISTS HERE */
+                String fromSquare = guiChessSquare.getId();
+                String toSquare = curSquare.getId();
+                san = guiChessSquare.getId() + curSquare.getId();       // get the move in terms of SAN (e.g. e3d6)
 
-                san = guiChessSquare.getId() + curSquare.getId();      // get the move in terms of SAN (e.g. e3d6)
-                curSquare.getChildren().remove(0);                     // remove the current chess piece
-                curSquare.getChildren().add(0, guiChessPiece);         // insert the first-click piece onto this square
+                //if (stockfish.isLegalMove()) {
+                if (true) {
+                    curSquare.getChildren().remove(0);                  // remove the current chess piece
+                    curSquare.getChildren().add(0, guiChessPiece);      // insert the first-click piece onto this square
 
-                // logic to update the 2D array
-                // ............
-
+                    update2DArrayChessboard(fromSquare, toSquare);      // keep 2D chessboard array updated
+                }
             }
 
             // finished with second-click
-            isFirstClick = true;                // back to start
+            isFirstClick = true;                                        // back to start
             guiChessPiece.setOpacity(1);        // opacity set back to show finished
         }
     }
 
-    public Pane getNodeByRowColumnIndex(final int row, final int column, GridPane gridPane) {
-        Pane result = null;
-        ObservableList<Node> children = gridPane.getChildren();
+    /**
+     * Takes two SAN coordinates and moves the chess piece from the start square to
+     *      the end square within the 2D chessboard array. Because of the way a chessboard
+     *      is "called", things get tricky due to [0][0] actually being referenced
+     *      as [a][8] in USCF terms.
+     *
+     * @param sanFromSquare The ID or SAN of the starting chess square. (e.g. 'a1')
+     * @param sanToSquare The ID or SAN of the ending chess square. (e.g. 'd6')
+     */
+    private void update2DArrayChessboard(final String sanFromSquare, final String sanToSquare) {
+        /*
+            Transform the SAN strings into 2D array coordinates, being careful due
+                to the USCF column layouts and 2D array column layouts are reversed.
+         */
+        int fromRow = sanTo2DRow(sanFromSquare);
+        int fromCol = sanTo2DCol(sanFromSquare);
+        int toRow = sanTo2DRow(sanToSquare);
+        int toCol = sanTo2DCol(sanToSquare);
 
-        for (Node node : children) {
-            if(gridPane.getRowIndex(node) == row && gridPane.getColumnIndex(node) == column) {
-                result = (Pane) node;
-                break;
-            }
-        }
-
-        return result;
+        chessboard[toRow][toCol] = chessboard[fromRow][fromCol];        // move piece from start to end square
+        chessboard[fromRow][fromCol] = 0;                               // the start square should now be empty
     }
+
+    /**
+     * Transforms a given SAN string coordinate (e.g. 'a1') to its corresponding
+     *      2D chessboard array ROW coordinate (e.g. '0'). Subtracted from (8) to
+     *      give the proper USCF layout.
+     *
+     * @param sanSquare A USCF chessboard coordinate that corresponds to a given chess square. (e.g. 'a1')
+     * @return The given chess square ROW within the 2D chessboard array. (e.g. '7')
+     */
+    private int sanTo2DRow (final String sanSquare) {
+        return (8 - ((int) sanSquare.charAt(1)) % 48);    // ASCII char (48) is '0'
+    }
+
+    /**
+     * Transforms a given SAN string coordinate (e.g. 'a1') to its corresponding
+     *      2D chessboard array COLUMN coordinate (e.g. '0').
+     *
+     * @param sanSquare A USCF chessboard coordinate that corresponds to a given chess square. (e.g. 'a1')
+     * @return The given chess square COLUMN within the 2D chessboard array. (e.g. '0')
+     */
+    private int sanTo2DCol (final String sanSquare) {
+        return ((int) sanSquare.toLowerCase().charAt(0)) % 97;     // ASCII chart (97) is 'a'
+    }
+
+    /**
+     * Given a SAN coordinate (e.g. 'a4'), will return the contents of the proper
+     *      2D array location (chess square).
+     *
+     * @param sanSquare The chessboard square, in SAN, to get the 2D array coordinates of. (e.g. 'a1')
+     * @return The contents of the given chess square; '0' if nothing exists on that square.
+     */
+    private char get2DArrayChessboardPiece(final String sanSquare) {
+
+        return chessboard[sanTo2DRow(sanSquare)][sanTo2DCol(sanSquare)];
+    }
+
+//    public Pane getNodeByRowColumnIndex(final int row, final int column, GridPane gridPane) {
+//        Pane result = null;
+//        ObservableList<Node> children = gridPane.getChildren();
+//
+//        for (Node node : children) {
+//            if(gridPane.getRowIndex(node) == row && gridPane.getColumnIndex(node) == column) {
+//                result = (Pane) node;
+//                break;
+//            }
+//        }
+//
+//        return result;
+//    }
 
     public static String sanToFenstring(String coordinates){
         return coordinates;
