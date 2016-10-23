ee930a13bf68516edc489a765b772e22600ae02b
David Bickford <drb56@pitt.edu>
2016-10-23 12:30:05 -0400

    cleaing up and adding packages

--- LaboonChess/src/main/java/LaboonChessDocumentController.java
+++ LaboonChess/src/main/java/LaboonChessDocumentController.java
@@ -300,19 +300,4 @@ public class LaboonChessDocumentController implements Initializable {
 //
 //    }
 
-
-//    public String chessboardToFEN(char[][] chessboard){
-//        char[] letterLocs = new char[]{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'};
-//        char[] numberLocs = new char[]{'8', '7', '6', '5', '4', '3', '2', '1'};
-//        String currBoard = "";
-//
-//        for(int i=0; i<chessboard.length; i++){
-//            for(int j=0; j<chessboard.length; j++){
-//                if(chessboard[i][j] != 0){
-//                    currBoard = currBoard + chessboard[i][j] + letterLocs[j] + numberLocs[i] + " ";
-//                }
-//            }
-//        }
-//        return currBoard;
-//    }
 }
