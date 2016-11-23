26f3e2136f106c793d91d8e6933e41686de4c760
Joe Meszar <mesz@outk.st>
2016-11-23 13:43:54 -0500

    Removed unused code and import statements from LaboonChess.java

--- LaboonChess/src/main/java/LaboonChess.java
+++ LaboonChess/src/main/java/LaboonChess.java
@@ -1,22 +1,16 @@
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import entities.TimerAndMessages;
 
 /**
  *
  * @author Joe Meszar (jwm54@pitt.edu)
  */
 public class LaboonChess extends Application {
-    @FXML Label lblTimer = null;
-    long timer_count = 0;
 
-    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/LaboonChessDocument.fxml"));
